473a5044f0d632dc8960db7df4d645070dba758e
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-19 13:15:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let FirstName=document.getElementById('First Name')
+let LastName=document.getElementById('Last Name')
+let PhoneNumber=document.getElementById('Phone Number')
+let EmailID=document.getElementById('Email ID')
+
+function popup(e){
+	e.preventDefault()
+	alert(`First Name:${FirstName.value} Last Name:${LastName.value} Phone Number:${PhoneNumber.value} Email ID:${EmailID.value}   `)
+}
